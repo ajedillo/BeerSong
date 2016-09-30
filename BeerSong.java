2823e4b76fb484e962819b408b443e29e8e1b811
andrew.edillo@gmail.com <andrew.edillo@gmail.com>
2016-09-30 18:51:50 -0400

    Implemented Ninety_Nine_Bottles_of_Beer() function.

--- BeerSong.java
+++ BeerSong.java
@@ -14,7 +14,23 @@ public class BeerSong {
     }
     
     public static String Ninety_Nine_Bottles_of_Beer() {
-        
+        String str1 = "";
+        for (int i = 99; i >= 0; i--) {
+            int j = i - 1;
+            if (i == 2) {
+                str1 += "2 bottles of beer on the wall, 2 bottles of beer,\n"
+                        + "Take one down, pass it around, 1 bottle of beer on the wall.\n";
+            } else if (i == 1) {
+                str1 += "1 bottle of beer on the wall, 1 bottle of beer,\n"
+                        + "Take one down, pass it around, no more bottles of beer on the wall.\n";
+            } else if (i == 0) {
+                str1 += "The end!";
+            } else {
+                str1 += i + " bottles of beer on the wall, " + i + " bottles of beer,\n"
+                        + "Take one down, pass it around, " + j + " bottles of beer on the wall.\n";
+            }
+        }
+        return str1;
     }
     
 }
