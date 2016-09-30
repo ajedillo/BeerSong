d008d91a1c412257accae9f533ab088553c71104
andrew.edillo@gmail.com <andrew.edillo@gmail.com>
2016-09-30 18:29:28 -0400

    added java file containing empty main and print functions

---
+++ BeerSong.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author Andrew Edillo <andrew.edillo@gmail.com>
+ * Main function calls Ninety_Nine_Bottles_of_Beer() function to print the
+ * "99 Bottles of Beer" song. Through this exercise, you will enhance your java
+ * programming skills further including GIT version control and problem solving. 
+ * Assume that your console is a wall where the song will be written once your 
+ * code compiles and executes.
+ */
+public class BeerSong {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    public static String Ninety_Nine_Bottles_of_Beer() {
+        
+    }
+    
+}
